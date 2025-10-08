42fb2784a1c38ae7cffc7de911dd793eb40ba076
Hitiksha Jagani <hitikshapatel191003@gmail.com>
2025-10-08 18:55:56 +0530

    Request controller for admin added

--- backend/src/main/java/com/service_booking_system/service/repository/ServiceProviderRepository.java
+++ backend/src/main/java/com/service_booking_system/service/repository/ServiceProviderRepository.java
@@ -2,10 +2,12 @@
 
 package com.service_booking_system.service.repository;
 
+import com.service_booking_system.service.enums.Status;
 import com.service_booking_system.service.model.ServiceProvider;
 import com.service_booking_system.service.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
@@ -13,4 +15,6 @@ public interface ServiceProviderRepository extends JpaRepository<ServiceProvider
     Optional<ServiceProvider> getByUser(Users user);
 
     ServiceProvider findByUser(Users user);
+
+    List<ServiceProvider> findByStatus(Status status);
 }
